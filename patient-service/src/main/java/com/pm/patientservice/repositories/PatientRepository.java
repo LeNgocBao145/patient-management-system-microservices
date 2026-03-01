65989fea979eae6882020735f86945e3d4fe5f7d
LeNgocBao145 <himagito@gmail.com>
2026-03-01 21:38:26 +0700

    feat: Patient Repository

---
+++ patient-service/src/main/java/com/pm/patientservice/repositories/PatientRepository.java
@@ -0,0 +1,13 @@
+package com.pm.patientservice.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pm.patientservice.models.Patient;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, UUID>{
+
+}
