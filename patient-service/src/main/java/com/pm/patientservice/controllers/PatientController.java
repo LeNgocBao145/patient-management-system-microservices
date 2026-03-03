6bfbb5646360fc33d9376f85959375241cfbcfed
LeNgocBao145 <himagito@gmail.com>
2026-03-03 20:24:12 +0700

    feat: Create patient controller

--- patient-service/src/main/java/com/pm/patientservice/controllers/PatientController.java
+++ patient-service/src/main/java/com/pm/patientservice/controllers/PatientController.java
@@ -3,13 +3,20 @@ package com.pm.patientservice.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.services.PatientService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/patients")
@@ -22,5 +29,11 @@ public class PatientController {
 		List<PatientResponseDTO> patients = patientService.getPatients();
 		
 		return ResponseEntity.ok().body(patients);
+	}
+	
+	@PostMapping
+	public ResponseEntity<PatientResponseDTO> createPatient(@Valid @RequestBody PatientRequestDTO patientRequestDTO){		
+		PatientResponseDTO patientResponseDTO = patientService.createPatient(patientRequestDTO);
+		return ResponseEntity.status(HttpStatus.CREATED).body(patientResponseDTO);
 	}
 }
