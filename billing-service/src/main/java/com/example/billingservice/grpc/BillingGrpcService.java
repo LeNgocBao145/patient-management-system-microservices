31fe1e8879c3ad0c91f870bc7b6beea371940c7e
LeNgocBao145 <himagito@gmail.com>
2026-03-06 19:00:01 +0700

    feat: patient service implement grpc client

--- billing-service/src/main/java/com/example/billingservice/grpc/BillingGrpcService.java
+++ billing-service/src/main/java/com/example/billingservice/grpc/BillingGrpcService.java
@@ -4,9 +4,11 @@ import billing.BillingRequest;
 import billing.BillingResponse;
 import billing.BillingServiceGrpc;
 import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@GrpcService
 public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBase {
     private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
 
